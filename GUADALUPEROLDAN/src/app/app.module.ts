19063827da9fffc4f292e5eaac60436bfbf91511
MilagrosRS <analiaelisabeths@gmail.com>
2023-01-11 04:14:29 -0300

    diseño

--- GUADALUPEROLDAN/src/app/app.module.ts
+++ GUADALUPEROLDAN/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HearderComponent } from './components/hearder/hearder.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HearderComponent,
+    LogoAPComponent
   ],
   imports: [
     BrowserModule,
